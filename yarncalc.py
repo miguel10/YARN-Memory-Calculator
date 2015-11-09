095c738d8c9f73becb501ec5f4f906c60d978966
Miguel <miguellucero10@gmail.com>
2015-11-09 13:49:22 -0700

    Update yarncalc.py

--- yarncalc.py
+++ yarncalc.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author: Miguel Lucero ( @miguellucero / http://toyelephants.wordpress.com ) 
+# Author: Miguel Lucero ( @miguellucero ) 
 # 
 # yarncalc.py - Script to calculate YARN Memory Configuration
 #
